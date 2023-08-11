f7f0827b8c8caf574dd4c5057d598718746f43ce
Garrett Girod <girodg@hotmail.com>
2023-08-11 09:35:41 -0400

    removing keys and their values

--- Day06/Day06/Program.cs
+++ Day06/Day06/Program.cs
@@ -26,16 +26,17 @@ namespace Day06
                 You do NOT need to check if the key is in dictionary. Check the returned bool.
                
             */
-            Dictionary<Weapon, int> backpack = new Dictionary<Weapon, int>()
+            Dictionary<Weapon, int> dorasbackpack = new Dictionary<Weapon, int>()
             {
                 {Weapon.Sword, 5 }
             };
-            backpack.Add(Weapon.Axe, 2);
-            backpack[Weapon.Spear] = 1;
+            dorasbackpack.Add(Weapon.Axe, 2);
+            dorasbackpack[Weapon.Spear] = 1;
 
 
-            bool wasFound = backpack.Remove(Weapon.Mace);
+            bool wasFound = dorasbackpack.Remove(Weapon.Mace);
             if (!wasFound) Console.WriteLine($"{Weapon.Mace} was NOT found.");
+            else Console.WriteLine("Mace was removed.");
 
 
 
@@ -56,7 +57,39 @@ namespace Day06
             Dictionary<string, double> grades = new();
             foreach (var student in students)
                 grades.Add(student, rando.NextDouble() * 100);
-            
+
+            do
+            {
+                PrintGrades(grades);
+                Console.Write("Student to drop: ");
+                string student = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(student)) break;
+
+                if (grades.Remove(student))
+                {
+                    Console.WriteLine($"{student} was dropped.");
+                }
+                else
+                    Console.WriteLine($"{student} is not in PG2 this month.");
+            } while (true);
+        }
+
+        private static void PrintGrades(Dictionary<string, double> grades)
+        {
+            Console.WriteLine($"\n\n    PG2 {DateTime.Now.Month} {DateTime.Now.Year}    ");
+            foreach (KeyValuePair<string, double> student in grades)
+            {
+                string name = student.Key;
+                double grade = student.Value;
+                Console.ForegroundColor = (grade < 59.5) ? ConsoleColor.Red :
+                                          (grade < 69.5) ? ConsoleColor.DarkYellow :
+                                          (grade < 79.5) ? ConsoleColor.Yellow :
+                                          (grade < 89.5) ? ConsoleColor.Blue :
+                                                           ConsoleColor.Green;
+                Console.Write($"{grade,7:N2} ");//N2 is a number w/ 2 decimal places formatting specifier
+                Console.ResetColor();
+                Console.WriteLine(name);
+            }
         }
     }
 }
