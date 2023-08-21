26304a14a5c1e1b3588750dd12e1853368e8ddbb
Garrett Girod <girodg@hotmail.com>
2023-08-21 08:48:17 -0400

    overloading, extension methods

--- Day09/Day09/Program.cs
+++ Day09/Day09/Program.cs
@@ -1,15 +1,44 @@
 ﻿namespace Day09
 {
+    static class Extensions
+    {
+        public static void ColorPrint(this ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(message);
+            Console.ResetColor();
+        }
+    }
     internal class Program
     {
+        static string Concat(string msg, int num) //post-fix
+        {
+            return msg + num;
+        }
+        static string Concat(int num, string msg) //pre-fix
+        {
+            return num + msg;
+        }
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Gotham!");
+
+            string univ = Concat("Spiderverse-", 800);
+            univ = Concat(20, " Lives");
+
+            Console.Write($"You have ");
+            ConsoleColor.Red.ColorPrint(univ);
+            Console.WriteLine("WOW!");
 
             Calculator t1000 = new();
             int n1 = 5, n2 = 2;
             int sum = t1000.Sum(n1,n2);
             Console.WriteLine($"{n1} + {n2} = {sum}");
+            double d1 = 40, d2 = 10;
+            double sumD = t1000.Sum(d1, n2);
+
+            List<int> nums = new() { 1, 2, 3, 4 };
         }
     }
 
@@ -31,6 +60,11 @@
     class Calculator
     {
         public int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        public double Sum(double n1, double n2)
         {
             return n1 + n2;
         }
